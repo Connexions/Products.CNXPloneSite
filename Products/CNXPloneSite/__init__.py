ff9871e094896081fa273366f922b0e6f2c1d436
Edward Woodward <ed.woodward@rice.edu>
2010-01-26 20:28:48 +0000

    Removed Collection monkeypatch and moved it to Collection object.  Fixes #10039

--- Products/CNXPloneSite/__init__.py
+++ Products/CNXPloneSite/__init__.py
@@ -19,21 +19,6 @@ from setup import CNXSitePolicy
 this_module = sys.modules[ __name__ ]
 
 
-#Monkeypatch Collection.py to include the URL for a CNX specific help files. 
-from Products.RhaptosCollection.types import Collection
-
-coldesc = 'Optional: Choose a subtype for this collection. <a href="/help/reference/collection-subtypes">(help)</a>' 
-Collection.schema['collectionType'].widget.description=coldesc
-
-subjdesc = 'Select the subject categories that apply to this collection. <a href="/help/reference/subjects">(help)</a>'
-Collection.schema['subject'].widget.description=subjdesc 
-
-GoogleAnalyticsTrackingCodeDesc = 'Enter the Google Analytics Tracking Code (e.g. UA-xxxxxxx-x) for this module to track its usage.<br/><em>Note that this code will track only the collection home page, not the modules therein.</em> <a href="/help/reference/GoogleAnalyticsTrackingCode">(help)</a>'
-Collection.schema['GoogleAnalyticsTrackingCode'].widget.description=GoogleAnalyticsTrackingCodeDesc
-
-AbstractDesc = "Enter a summary of the collection and/or its objectives. May contain a limited set of CNXML. <a href='/help/authoring/authorguide#summary'>(help)</a>"
-Collection.schema['abstract'].widget.description=AbstractDesc
-
 # Make the skins available as DirectoryViews
 registerDirectory('skins', globals())
 
