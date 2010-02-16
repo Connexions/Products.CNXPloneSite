d5c7177162cb10ab314f91fd8469505f02682ab1
Edward Woodward <ed.woodward@rice.edu>
2010-02-16 20:23:00 +0000

    Updated Contact Us info and included IBC building map. Refs #10171

--- Products/CNXPloneSite/setup/CNXSetup.py
+++ Products/CNXPloneSite/setup/CNXSetup.py
@@ -189,6 +189,17 @@ def createAboutusFolder(self, portal):
         text = ifile.read()
         ifile.close()
         contact.edit('html', text)
+        
+        #add ibc map to about us
+        folder.invokeFactory('Image', 'ibc-map.png')
+        imageFolder = folder.ibc-map.png
+        ifile = open(
+            os.path.join(os.path.dirname(__file__), 'data', 'ibc-map.png'),
+            'rb')
+        image = ifile.read()
+        ifile.close()
+        imageFolder.edit('image/png', image)
+        
         folder.invokeFactory('Document', 'index_html')
         index = folder.index_html
         index.setTitle('Philosophy')
